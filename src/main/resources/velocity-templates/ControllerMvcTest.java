65289a9400cc4dcdd333df2dc3674b6df2835063
rmjames <robert-miller-1@uiowa.edu>
2014-10-09 16:30:54 -0500

    added save MVC test method

--- src/main/resources/velocity-templates/ControllerMvcTest.java
+++ src/main/resources/velocity-templates/ControllerMvcTest.java
@@ -9,11 +9,14 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import edu.uiowa.icts.aptamer.domain.JobType;
 import ${basePackageName}.dao.*;
 import ${basePackageName}.domain.*;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -83,36 +86,47 @@ public class ${className}ControllerMvcTest extends AbstractControllerMVCTests {
        .andExpect(view().name("${jspPath}/list_alt"));
     }
     
+    
       #if ( ${domainClass.isUsesCompositeKey()} ) 
     	// at the moment, don't test datatables for composite keys 
       #elseif ( ${domainClass.getPrimaryKey().getType()} && ${domainClass.getPrimaryKey().getType()} != "Integer" )
         // at the moment, don't test datatables for ids generated without sequences  
       #else	  
-    	    @Test
-    	    public void defaultDatatableShouldReturnJSONDataWith10Rows() throws Exception {
-    	    	mockMvc.perform(get("${pathPrefix}/datatable")
-    	    			.param("display", "list")
-    	    			.param("search[value]", "")
-    	    			.param("search[regex]", "false")
-    	    			.param("length", "10")
-    	    			.param("start", "0")
-    	    			.param("columnCount", "${columnNamesList.size()}")
-    	    			.param("draw", "1")
-    	    			.param("individualSearch", "true")
-    	    			#foreach( $columnName in $columnNamesList )
-    	    			   #set( $arrayIndex = $foreach.count - 1 )
-    					  .param("columns[${arrayIndex}][data]","${arrayIndex}").param("columns[${arrayIndex}][name]","${columnName}").param("columns[${arrayIndex}][searchable]","true").param("columns[${arrayIndex}][orderable]","true").param("columns[${arrayIndex}][search][regex]","false").param("columns[${arrayIndex}][search][value]","")
-    	    			#end    	    			
-    					.accept(MediaType.APPLICATION_JSON))
-    	    	.andExpect(status().isOk())
-    	    	.andExpect(content().contentType("application/json"))
-    	    	.andExpect(jsonPath("$.recordsTotal", is(${daoServiceName}.get${className}Service().list().size())))
-    	    	.andExpect(jsonPath("$.recordsFiltered", is(${daoServiceName}.get${className}Service().list().size())))
-    	    	.andExpect(jsonPath("$.draw", is("1")))
-    	    	// max # of returned data rows should be 10 per "length" value
-    	    	.andExpect(jsonPath("$.data", hasSize(is(10))))
-    	        ;
-    	    }
+    	  
+    @Test
+    public void saveShouldPersistAndRedirectToListView() throws Exception {
+       int count = ${daoServiceName}.get${className}Service().list().size();
+       
+       mockMvc.perform(post("${pathPrefix}/save")).andExpect(status().is3xxRedirection()).andExpect(view().name("redirect:${pathPrefix}/list${pathExtension}"));   
+       
+       assertEquals("${className} count should increase by 1", count +1 , ${daoServiceName}.get${className}Service().list().size());
+	}
+  
+    @Test
+    public void defaultDatatableShouldReturnJSONDataWith10Rows() throws Exception {
+    	mockMvc.perform(get("${pathPrefix}/datatable")
+			.param("display", "list")
+			.param("search[value]", "")
+			.param("search[regex]", "false")
+			.param("length", "10")
+			.param("start", "0")
+			.param("columnCount", "${columnNamesList.size()}")
+			.param("draw", "1")
+			.param("individualSearch", "true")
+			#foreach( $columnName in $columnNamesList )
+			   #set( $arrayIndex = $foreach.count - 1 )
+			  .param("columns[${arrayIndex}][data]","${arrayIndex}").param("columns[${arrayIndex}][name]","${columnName}").param("columns[${arrayIndex}][searchable]","true").param("columns[${arrayIndex}][orderable]","true").param("columns[${arrayIndex}][search][regex]","false").param("columns[${arrayIndex}][search][value]","")
+			#end    	    			
+			.accept(MediaType.APPLICATION_JSON))
+    	.andExpect(status().isOk())
+    	.andExpect(content().contentType("application/json"))
+    	.andExpect(jsonPath("$.recordsTotal", is(${daoServiceName}.get${className}Service().list().size())))
+    	.andExpect(jsonPath("$.recordsFiltered", is(${daoServiceName}.get${className}Service().list().size())))
+    	.andExpect(jsonPath("$.draw", is("1")))
+    	// max # of returned data rows should be 10 per "length" value
+    	.andExpect(jsonPath("$.data", hasSize(is(10))))
+        ;
+    }
     	  
     	  
       #end
